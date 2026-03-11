a95e7e934f21bc303ce63ed65338f0d4b95bcca3
Yauheni Charniauski <chernyavskij.z@mail.ru>
2026-03-11 21:35:10 +0300

    laba3

--- Laba1/app/build.gradle.kts
+++ Laba1/app/build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.compose)
+
+    // Только KSP
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17"
 }
 
 android {
     namespace = "com.example.laba1"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.laba1"
@@ -28,10 +28,12 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     buildFeatures {
         compose = true
     }
@@ -47,11 +49,17 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
+
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
---
+++ Laba1/app/src/main/java/com/example/laba1/RecipeDao.kt
@@ -0,0 +1,37 @@
+package com.example.laba1
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Update
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Transaction
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface RecipeDao {
+
+    @Transaction
+    @Query("SELECT * FROM recipes ORDER BY title ASC")
+    fun getAllRecipesWithSteps(): Flow<List<RecipeWithSteps>>
+
+    @Transaction
+    @Query("SELECT * FROM recipes WHERE id = :id")
+    fun getRecipeWithSteps(id: Int): Flow<RecipeWithSteps>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertRecipe(recipe: RecipeEntity): Long
+
+    @Insert
+    suspend fun insertSteps(steps: List<RecipeStepEntity>)
+
+    @Update
+    suspend fun updateRecipe(recipe: RecipeEntity)
+
+    @Delete
+    suspend fun deleteRecipe(recipe: RecipeEntity)
+
+    @Query("DELETE FROM recipe_steps WHERE recipeId = :recipeId")
+    suspend fun deleteStepsForRecipe(recipeId: Int)
+}
